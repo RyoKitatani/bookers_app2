42f35546dd7945636e6f0fa45877b18f3c7b9285
RyoKitatani <ryo0929m@gmail.com>
2021-02-20 15:15:16 +0000

    createアクション作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,13 +2,17 @@ class BooksController < ApplicationController
     
  def index
      @books = Book.all
- end
- 
- def new
      @book = Book.new
  end
  
+ def create
+     book = Book.new(book_params)
+     book.save
+     redirect_to books_path
+ end
+ 
  def show
+     @book = Book.find(params[:id])
  end
  
  def edit
@@ -21,6 +25,11 @@ class BooksController < ApplicationController
  
  def destroy
      
+ end
+ 
+ private
+ def book_params
+    params.require(:book).permit(:title, :body)
  end
     
 end
